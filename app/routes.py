65db7e35f684538c242603dbce0d6833e1544e27
acuba001 <acuba001@fiu.edu>
2018-10-05 13:10:54 -0700

    Added random page.  It just prints a number from 0 to 10 inclusive.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from random import randint
 
 @app.route('/')
 @app.route('/index')
@@ -22,12 +23,12 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print('Im In!')
         flash('Login requested for user {}, remember_me={}'
         .format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    print('submitted {}'.format(form.is_submitted()))
-    print('validate {}'.format(form.validate()))
-    print(form.password.errors)
-    print('Im Out!')
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/random')
+def rand():
+    r = randint(0, 10)
+    return render_template('random.html', r=str(r))

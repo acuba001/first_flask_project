943c55211b406c8174b83b8d45f2f079330ca734
acuba001 <4492431+acuba001@users.noreply.github.com>
2018-11-20 14:27:20 -0800

    Added server-side and admin email logging. Also added error pages for 404 and 500. Fixed edit_profile validation for unique username.

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,42 @@ migrate = Migrate(app=app, db=db)
 login = LoginManager(app=app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
+if not app.debug:
+
+    # Email admins when there is an error
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs = app.config['ADMINS'],
+            subject = 'First Flask Server Failure',
+            credentials = auth,
+            secure = secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # Log errors on server
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/first_flask_project.log', \
+                maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('first_flask_project startup')
--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class Config(object):
 
     # the feature for notifying evertime when the database changes
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [os.environ.get('MY_ADMIN_EMAIL')]

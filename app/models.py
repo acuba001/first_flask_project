0636c1c5a70a2a203f18db32a7c8868964c32dfe
acuba001 <4492431+acuba001@users.noreply.github.com>
2018-11-23 21:10:32 -0800

    Implemented 'follow' functionality. An auxiliary table was made for the many-to-many relationship. A unit test was created.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -13,6 +19,14 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', 
+        secondary=followers, 
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'),
+        lazy = 'dynamic'
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -28,6 +42,38 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'\
             .format(digest, size)
 
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def followed_posts(self):
+        followed_posts = Post.query.join(
+            followers, 
+            followers.c.followed_id == Post.user_id
+        ).filter(
+            followers.c.follower_id == self.id
+        )
+        
+        own_posts = Post.query.filter_by(user_id=self.id)
+
+        return followed_posts.union(
+            own_posts
+            ).order_by(
+                Post.timestamp.desc()
+                )
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,7 +83,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))

597063eda9bc05b665a585e64ececf736e8fd862
acuba001 <4492431+acuba001@users.noreply.github.com>
2018-12-16 20:18:36 -0800

    Added flask-bootstap and bootstrap to the templates.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(obj=Config)
@@ -12,6 +13,7 @@ migrate = Migrate(app=app, db=db)
 login = LoginManager(app=app)
 login.login_view = 'login'
 mail = Mail(app=app)
+bootstrap = Bootstrap(app=app)
 
 from app import routes, models, errors
 import logging
